f73899e93dbc27cb4834a87b51d7e458f93362a6
Jamilly Quirino <jamilly.quirino@academico.ifpb.edu.br>
2025-04-15 10:05:12 -0300

    definição de uma rota e de um cabeçalho padrão

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,6 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <header><h1>Jojo</h1></header>
         {children}
       </body>
     </html>
---
+++ src/app/usuario/page.js
@@ -0,0 +1,6 @@
+export default () => (
+    <>
+      <h1>Fulano de tal</h1>
+      <p>jojo todinho</p> 
+    </>
+)
