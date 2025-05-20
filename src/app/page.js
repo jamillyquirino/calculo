296343a3d274800fefecd9a7f61c75961b2a79dd
Jamilly Quirino <jamilly.quirino@academico.ifpb.edu.br>
2025-05-20 11:32:45 -0300

    texto5

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from './page.module.css';
 import Link from 'next/link';
+import Caixa2 from '../componentes/caixa2.js';
 
 export default function PaginaInicial() {
   return (
@@ -8,12 +9,32 @@ export default function PaginaInicial() {
       <div style={{ display: 'flex', justifyContent: "flex-end" }}>
         <Link href="/usuario" className={styles.circle}>Perfil</Link>
       </div>     
+
       <div className={styles.abaLateral}></div>
+
       <div className={styles.conteudo}> 
         <h2 className={styles.tituloPrincipal}>ACESSO RÁPIDO</h2>
+
+        {/* Caixinha de desempenho */}
+        <div className={styles.caixaDesempenho}>
+          <h3 className={styles.tituloCaixa}>VISÃO GERAL DO SEU DESEMPENHO</h3>
+          <div className={styles.metricas}>
+            <div className={styles.item}>
+              <div className={styles.subtitulo}>Taxa de Acertos</div>
+              <div className={styles.percentualAcerto}>56%</div>
+              <p className={styles.detalhe}>Você acertou 51 questões</p>
+              <div className={styles.iconeVerde}>✔</div>
+            </div>
+            <div className={styles.item}>
+             {/* <div className={styles.subtitulo}>Taxa de Erros</div> 
+              <div className={styles.percentualErro}>44%</div> 
+              <p className={styles.detalhe}>Você errou 40 questões</p>
+              <div className={styles.iconeVermelho}>✘</div>*/}
+            <Caixa2/>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className={styles.caixa}></div>
-      
     </div>
   );
 }
---
+++ src/componentes/caixa2.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from '../app/page.module.css';
+import Link from 'next/link';
+
+export default function App() {
+  return (
+    <div className={styles.card}></div>
+  );
+}
+
